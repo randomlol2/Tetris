d176f560d267af177cb123365627c6bdf10ef51c
Howard Halim <howardhalim@gmail.com>
2018-05-25 11:07:30 -0400

    Test commit

--- Main.java
+++ Main.java
@@ -1,9 +1,5 @@
-
-
-public class Main
-{
-	public static void main ( String[] args )
-	{
-		ProgramWindow frame = new ProgramWindow ();
+public class Main {
+	public static void main(String[] args) {
+		ProgramWindow frame = new ProgramWindow();
 	}
 }
