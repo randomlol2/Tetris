77847694afcffdb5d468f5e28d0515a3800ed71f
Howard Halim <howardhalim@gmail.com>
2018-05-31 17:01:32 -0400

    Restart works now

--- Tetris.java
+++ Tetris.java
@@ -25,17 +25,7 @@ public class Tetris extends Panel implements KeyListener
 	double moveTime = 0.8; // time between each block movement, in seconds
 	Boolean died = false;
 	int score = 0;
-	Timer timer = new Timer();
-
-	TimerTask Fall = new TimerTask() {
-		public void run() {
-			if (died)
-				return;
-			if(!activePiece.shiftDown())
-				newPiece();
-			repaint();
-		}
-	};
+	Timer timer;
 
 	public Tetris()
 	{
@@ -57,6 +47,17 @@ public class Tetris extends Panel implements KeyListener
 			}
 		}
 		newPiece();
+
+		TimerTask Fall = new TimerTask() {
+			public void run() {
+				if (died)
+					return;
+				if(!activePiece.shiftDown())
+					newPiece();
+				repaint();
+			}
+		};
+		timer = new Timer();
 		timer.scheduleAtFixedRate(Fall, 0, (long)(moveTime*1000));
 		died = false;
 		score = 0;
@@ -90,6 +91,7 @@ public class Tetris extends Panel implements KeyListener
 		}
 		activePiece.display(osg, s);
 		osg.setColor(Color.WHITE);
+		// osg.setFont(new Font("Arial", Font.PLAIN, 8));
 		if (died)
 		{
 			osg.drawString("Game Over! Youre score is " + score, 50, 50);
@@ -111,7 +113,7 @@ public class Tetris extends Panel implements KeyListener
 					print("You Lost");
 					died = true;
 					timer.cancel();
-					timer = new Timer();
+					repaint();
 					return;
 				}
 			}
@@ -185,7 +187,8 @@ public class Tetris extends Panel implements KeyListener
 									break;
 			case KeyEvent.VK_X: 	activePiece.rotate(1);
 									break;
-			case KeyEvent.VK_R:		startGame();
+			case KeyEvent.VK_R:		timer.cancel();
+									startGame();
 									break;
 		}
 		repaint();
@@ -194,3 +197,166 @@ public class Tetris extends Panel implements KeyListener
 	public void keyTyped(KeyEvent e){}
 	public void keyReleased(KeyEvent e){}
 }
+
+
+// import java.awt.*;
+// import java.awt.event.*;
+// import java.util.*;
+// import java.awt.image.*;
+//
+// public class Tetris extends Panel implements KeyListener
+// {
+// 	// for convenience
+// 	public void print(String s){
+// 		System.out.println(s);
+// 	}
+// 	public void print(int s){
+// 		System.out.println(s);
+// 	}
+//
+// 	Dimension dim = null;
+// 	int[][] board;
+// 	int rows = 24; // number of rows in board
+// 	int cols = 12; // number of columns in board
+// 	int s; // side length of square in board
+// 	Color[] colors = new Color[]{Color.BLACK, Color.BLUE, Color.WHITE, Color.RED, Color.GREEN, Color.YELLOW, Color.ORANGE, Color.MAGENTA, Color.GRAY}; // colors for the board
+// 	Pieces activePiece;
+// 	BufferedImage osi; // off screen image
+// 	Graphics osg; // off screen graphics
+// 	double moveTime = 0.8; // time between each block movement, in seconds
+// 	Boolean playing = true;
+//
+// 	TimerTask Fall = new TimerTask() {
+// 		public void run() {
+// 			if(!activePiece.shiftDown())
+// 				newPiece();
+// 			repaint();
+// 		}
+// 	};
+//
+// 	public Tetris()
+// 	{
+// 		addKeyListener(this);
+// 		board = new int[rows][cols];
+// 		for(int i = 0; i < rows; i++)
+// 		{
+// 			for (int j = 0; j < cols; j++)
+// 			{
+// 				if (j == 0 || i == rows - 1 || j == cols - 1)
+// 					board[i][j] = 8; // border (grey)
+// 				else
+// 					board[i][j] = 0; // empty (black)
+// 			}
+// 		}
+// 		newPiece();
+// 		Timer timer = new Timer();
+// 		timer.scheduleAtFixedRate(Fall, 0, (long)(moveTime*1000));
+// 	}
+//
+// 	public void paint(Graphics g)
+// 	{
+// 		dim = getSize();
+// 		osi = new BufferedImage(dim.width, dim.height, BufferedImage.TYPE_INT_RGB);
+// 		osg = osi.getGraphics();
+// 		update(g);
+// 	}
+//
+// 	public void update(Graphics g)
+// 	{
+// 		deleteRows();
+// 		int width = dim.width;
+// 		int height = dim.height;
+// 		s = Math.min(height/rows, width/cols);
+// 		for (int i = 0; i < rows; i++)
+// 		{
+// 			for (int j = 0; j < cols; j++)
+// 			{
+// 				osg.setColor(colors[board[i][j]]);
+// 				if (i < 4)
+// 					osg.setColor(Color.GRAY); // don't show the first 4 rowss
+// 				osg.fillRect(j*s, i*s, s, s);
+// 				osg.setColor(Color.GRAY);
+// 				osg.drawRect(j*s, i*s, s, s);
+// 			}
+// 		}
+// 		activePiece.display(osg, s);
+// 		g.drawImage(osi, 0, 0, this);
+// 	}
+//
+// 	public void checkDeath()
+// 	{
+// 		for (int i = 0; i<4; i++)
+// 		{
+// 			for (int j = 1; j < cols-1; j++)
+// 			{
+// 				if (board[i][j] != 0)
+// 				{
+// 					print("You Lost");
+// 					playing = false;
+// 				}
+// 			}
+// 		}
+// 	}
+//
+// 	public void newPiece()
+// 	{
+// 		checkDeath();
+// 		if (!playing)
+// 			return;
+// 		int type = (int)(7*Math.random())+1; // random int between 1 and 7
+// 		activePiece = new Pieces(type, colors[type], board);
+// 	}
+//
+// 	public void deleteRows()
+// 	{
+// 		for (int i = 0; i < rows - 1; i++)
+// 			while (deleteRow(i));
+// 	}
+//
+// 	public Boolean deleteRow(int row)
+// 	{
+// 		Boolean full = true;
+// 		for (int j = 1; j < cols-1; j++)
+// 		{
+// 			if (board[row][j] == 0)
+// 				full = false;
+// 		}
+// 		if (full)
+// 		{	// shift everything abodfve row down by one
+// 			for (int i = row; i > 0; i--)
+// 			{
+// 				for (int j = 1; j < cols-1; j++)
+// 					board[i][j] = board[i-1][j];
+// 			}
+// 			for (int j = 1; j < cols-1; j++)
+// 				board[0][j] = 0;
+// 		}
+// 		return full;
+// 	}
+//
+// 	// KeyListener interface methods
+// 	public void keyPressed(KeyEvent e)
+// 	{
+// 		switch (e.getKeyCode())
+// 		{
+// 			case KeyEvent.VK_LEFT: 	activePiece.move(0);
+// 									break;
+// 			case KeyEvent.VK_RIGHT: activePiece.move(1);
+// 									break;
+// 			case KeyEvent.VK_DOWN: 	if(!activePiece.shiftDown())
+// 										newPiece();
+// 									break;
+// 			case KeyEvent.VK_SPACE: activePiece.move(2);
+// 									newPiece();
+// 									break;
+// 			case KeyEvent.VK_Z: 	activePiece.rotate(0); // rotate clockwise
+// 									break;
+// 			case KeyEvent.VK_X: 	activePiece.rotate(1);
+// 									break;
+// 		}
+// 		repaint();
+// 	}
+//
+// 	public void keyTyped(KeyEvent e){}
+// 	public void keyReleased(KeyEvent e){}
+// }
